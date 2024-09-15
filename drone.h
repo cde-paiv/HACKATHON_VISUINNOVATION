792017fb160a4eadd03318d5bc5997d0d8c360ca
root <root@DESKTOP-K5CFUMT>
2024-09-15 02:03:58 +0100

    Primeiro commit - adicionando arquivos do projeto

---
+++ drone.h
@@ -0,0 +1,22 @@
+#ifndef DRONE_DELIVER_H
+#define DRONE_DELIVER_H
+
+#include <PilotArdu.h>
+
+enum DroneAction {
+  TAKEOFF,
+  FLYTOA,
+  FLYTOB,
+  FLYTOC,
+  FLYTOD,
+  FLYTOE,
+  DELIVER,
+  RETURNTOHOME,
+  LAND,
+  EMERGENCYLAND
+};
+
+// Function prototypes
+
+
+#endif
