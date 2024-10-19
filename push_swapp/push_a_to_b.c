b13740a38ca50ba3a0778f46a910bef06d58c8cd
cde-paiv <motachristofer@gmail.com>
2024-10-19 22:01:49 +0100

    envio push_swap

--- push_swapp/push_a_to_b.c
+++ push_swapp/push_a_to_b.c
@@ -31,58 +31,56 @@ static void push_top(t_info *info, int i)
 // calcula o pivô básico, para dividir o conjunto em partes.
 static void get_pivot_1(t_info *info)
 {
-    t_stack *temp;
-    int min;
-    int max;
+	t_stack	*temp;
+	int		min;
+	int		max;
 
-    temp = info->stack_a;
-    while (temp)
-    {
-        if (temp->prev == NULL)
-        {
-            min = temp->index;
-            max = temp->index;
-        }
-        else
-        {
-            if (temp->index < min)
-                min = temp->index;
-            else if (temp->index > max)
-                max = temp->index;
-        }
-        temp = temp->next;
-    }
-    info->pivot = (min + max) / 2;
+	temp = info->stack_a;
+	while (temp)
+	{
+		if (temp->prev == NULL)
+		{
+			min = temp->index;
+			max = temp->index;
+		}
+		else
+		{
+			if (temp->index < min)
+				min = temp->index;
+			else if (temp->index > max)
+				max = temp->index;
+		}
+		temp = temp->next;
+	}
+	info->pivot = (min + max) / 2;
 }
 // lida com a divisão em blocos, ajustando o pivô dependendo do tamanho da pilha.
 static void get_pivot(t_info *info)
 {
-    static int  blocks;
-    static int  i;
-    static int  div;
+	static int	blocks;
+	static int	i;
+	static int	div = 1;
 
-    div = 1;
-    if (i == 0)
-    {
-        if (info->len_a >= 100 && info->len_a < 500)
-            blocks = 5;
-        else if (info->len_a >= 500)
-            blocks = 11;
-        else
-            blocks = 1;
-    }
-    if (blocks != 1 && i < info->len_a)
-    {
-        if (i >= (info->len_a / blocks) * div)
-        {
-            div++;
-            info->pivot = (info->len_a / blocks) * div;
-        }
-    }
-        else
-             get_pivot_1(info);
-        i++;
+	if (i == 0)
+	{
+		if (info->len_a >= 100 && info->len_a < 500)
+			blocks = 5;
+		else if (info->len_a >= 500)
+			blocks = 11;
+		else
+			blocks = 1;
+	}
+	if (blocks != 1 && i < info->len_a)
+	{
+		if (i >= (info->len_a / blocks) * div)
+			div++;
+		info->pivot = (info->len_a / blocks) * div;
+	}
+	else
+		get_pivot_1(info);
+	i++;
 }
+
 // decide qual operação realizar quando a pilha é grande.
 void    bigger_stacks(t_info *info)
 {
